169c6c4fcaaa882106e5ffa85ce729412c647f4a
Raffaello Damgaard <raffaello_neves@carrefour.com>
2025-05-22 22:31:22 -0300

    Build estático

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon']
+  modules: ['@nuxt/icon'],
+    ssr: false,
+  app: {
+    baseURL: '/',
+  },
+  nitro: {
+    preset: 'static',
+  }
 })
